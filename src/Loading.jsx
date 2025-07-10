9c80463fa3b0ac8ea000f1ec2e225ef3307f953a
amirshah07 <144581429+amirshah07@users.noreply.github.com>
2025-07-11 02:05:16 +0800

    added loading spinner component

---
+++ src/Loading.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+export default function Loading() {
+    return(
+        <>
+        <div className="loader-container">
+            <div className="loader"></div>
+        </div>
+        </>
+    )
+}
